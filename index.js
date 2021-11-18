c86a649c5496e8cac1ebdd6e4c84ee521bd93f32
jinnia <jinniaravindran@gmail.com>
2021-11-18 07:54:48 +0530

    added cors and urlencoded

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
-const port = 3000
+
+let corsOptions = {
+  origin:'http://localhost:3000'
+}
+require('dotenv').config()
+
+app.use(express.json()) //passing all api request to json
+app.use(cors(corsOptions))        //allowing diffrent domain access 
+app.use(express.urlencoded({extended:true})); //accepting all url encoded requests
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello rat!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.APP_PORT}`)
 })
