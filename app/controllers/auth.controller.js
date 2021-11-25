dc354c74adab838fec08bac10246189f6f7bb3b1
jinnia <jinniaravindran@gmail.com>
2021-11-25 08:34:26 +0530

    signup mdified with roles

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -5,20 +5,69 @@ const bcrypt = require('bcrypt')
 
 
 exports.signup = (req, res) => {
-   // res.status(200).send({ message: req.body }); 
+const user = new User({
+username: req.body.username,
+email: req.body.email,
+password: bcrypt.hashSync(req.body.password, 8),
+});
 
-  const user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8),
-  });
 
-  user.save((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
-    res.status(200).send({ message: 'successfully created user' });
-  });
 
+user.save((err, user) => {
+if (err) {
+res.status(500).send({ message: err });
+return;
+}
+
+
+
+if (req.body.roles) {
+Role.find(
+{
+name: { $in: req.body.roles },
+},
+(err, roles) => {
+if (err) {
+res.status(500).send({ message: err });
+return;
+}
+
+
+
+user.roles = roles.map((role) => role._id);
+user.save((err) => {
+if (err) {
+res.status(500).send({ message: err });
+return;
+}
+
+
+
+res.send({ message: "User was registered successfully!" });
+});
+}
+);
+} else {
+Role.findOne({ name: "user" }, (err, role) => {
+if (err) {
+res.status(500).send({ message: err });
+return;
+}
+
+
+
+user.roles = [role._id];
+user.save((err) => {
+if (err) {
+res.status(500).send({ message: err });
+return;
+}
+
+
+
+res.send({ message: "User was registered successfully!" });
+});
+});
+}
+});
 };
--- app/models/index.js
+++ app/models/index.js
@@ -2,15 +2,9 @@ const mongoose = require("mongoose")
 
 mongoose.Promise = global.Promise;
 
-
-
 const db = {};
 
-
-
 db.mongoose = mongoose;
-
-
 
 db.user = require("./user.model");
 db.role = require("./role.model");
